1906fbed25ca1f0a926ce92d450db9d41b933719
Daiane Galvão <daianegalvao@microsoft.com>
2025-11-05 19:40:49 -0300

    feat: altera função de soma adicionando parametro

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -1,5 +1,5 @@
 const sum = require('../src/sum');
 
-test('soma 2 + 2 para ser igual a 4', () => {
-  expect(sum(2, 2)).toBe(4);
+test('soma 2 + 2 + 1 para ser igual a 5', () => {
+  expect(sum(2, 2, 1)).toBe(5);
 });
